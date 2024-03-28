901bdbb3cbc400f86235fc1f63e9fb8aa423df12
Gabriela Colombo <gabriela-colombo15@outlook.com>
2024-03-28 14:16:05 -0300

    manipulação de strings

---
+++ Python Basico/strings.py
@@ -0,0 +1,22 @@
+# frase = "A Fulana é a melhor dev da empresa"
+
+# print(f"Upper(): {frase.upper()}")
+# print(f"Lower() - deixa tudo minusculo - {frase.lower()}")
+# print(f"replace() - substituir um valor na string - {frase.replace('da empresa', 'do Mundo')}")
+# print(f"split() - transforma em array - {frase.split('melhor')}")
+
+# frase_2 = "ksmdksamdskmsakmsakdm andsjsanjkdsnkjsa kmdskasmkamdskdsajnsdk /t k10209394030m kmsakAndsjsanjsnjasnjd /t ksamdkmdsksamkamdskdmdak /n kmsdkmdkmdakmdskm"
+# arr_frase_2 = frase_2.split('/t')
+# print(arr_frase_2)
+
+# frase_3 = "     olá mundo, tudo bem?      "
+
+# print(f"{frase + frase_3}")
+# print(f"rstrip(): {frase_3.rstrip()}")
+# print(f"lstrip():{frase_3.lstrip()}")
+# # print(frase_3 + "/n" + frase_3.strip()) 
+# print(frase_3 + "/n" + frase_3.strip()) #comentário
+
+
+a = "a x gosta de viajar"
+print(a[0] + " " + a[1] + " " + a[7:13: 2])
