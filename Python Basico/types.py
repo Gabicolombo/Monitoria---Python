87885ebafc504c9c6b69391628f73c5a1a2ac634
Gabriela Colombo <gabriela-colombo15@outlook.com>
2024-03-28 14:15:13 -0300

    tipos de dados

---
+++ Python Basico/types.py
@@ -0,0 +1,14 @@
+print("Olá Mundo")
+print('Olá Mundo 2')
+name = "Gabriela"
+
+print(f"Olá {name}")
+
+a = 5.6
+print(f'{a} é do tipo {type(a)}')
+b = ['Gabriela', 'Thamirys', 'Vania']
+print(f'{b} é do tipo {type(b)}')
+print(f'{name} é do tipo {type(name)}')
+
+print('Olá ' + name + ' tudo bem?')
+
