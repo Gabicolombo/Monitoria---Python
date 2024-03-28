f79d121d609b22311e3ced2a1159f6fb7899b540
Gabriela Colombo <gabriela-colombo15@outlook.com>
2024-03-28 14:15:45 -0300

    script que mostra os tipos de funções

---
+++ Python Basico/functions.py
@@ -0,0 +1,35 @@
+# print('começo da função')
+
+# def hello():
+#     print('Hello')
+
+# print('depois da função')
+
+# def soma(a, b, c=0):
+#     return a + b + c
+
+# print(soma(3, 4))
+# print(soma(1, 6, 9))
+
+# 5! = 5*4*3*2*1
+
+# def fatorial(n):
+#     if n == 0:
+#         return 1
+    
+#     return n * fatorial(n-1)
+
+# print(fatorial(6))
+
+# def funcao_recursiva():
+#     funcao_recursiva()
+
+# funcao_recursiva()
+
+def soma(a, b):
+    return a + b
+
+def celsius_para_fa(c):
+    return soma((c * (9/5)), 32)
+
+print(celsius_para_fa(0))
